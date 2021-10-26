b7b22b2d008402f971e3338821bea06eacdba4d1
do-oni <dxxneo@gmail.com>
2021-10-27 01:32:06 +0900

    Initial Commit

---
+++ src/test/java/kr/pe/whatsmap/WhatsmapApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pe.whatsmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
