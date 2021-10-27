5d00691ac9ace8295ce203269b7e9b36bb88b3d5
do-oni <dxxneo@gmail.com>
2021-10-28 01:42:42 +0900

    add react-app

---
+++ src/main/java/kr/pe/whatsmap/WhatsmapApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.whatsmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsmapApplication.class, args);
+	}
+
+}
