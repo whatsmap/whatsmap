10a05199235272733e138516d5bd71cacf62d38e
jiwonbbang <87870107+jiwonbbang@users.noreply.github.com>
2021-11-01 23:05:03 +0900

    [Add] Map.js

--- src/main/whatsmap/src/App.js
+++ src/main/whatsmap/src/App.js
@@ -1,25 +1,15 @@
 import './App.css';
-import React, { Component } from "react";
-
-// import Map from 'Map.js'
-
+import React, { Component } from 'react';
+import Map from './Map'
 
 class App extends Component{
   render(){
     return (
       <div className="App">
-        <p>TEST1app</p>
-TEST2app test3
-aaaaaa
-        {/* <Map>TEST3app</Map> */}
-  
+       <Map></Map>
       </div>
-
     );
   }
 }
-
-
-
 
 export default App;
