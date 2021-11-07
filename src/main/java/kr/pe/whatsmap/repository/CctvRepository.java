a7c53599d8feaf0c2c6223ebd370f83c1873c55d
jhshin29 <shinji03264@gmail.com>
2021-11-08 01:57:02 +0900

    [ëAdd] Test APi

--- src/main/java/kr/pe/whatsmap/repository/CctvRepository.java
+++ src/main/java/kr/pe/whatsmap/repository/CctvRepository.java
@@ -2,11 +2,13 @@ package kr.pe.whatsmap.repository;
 
 import kr.pe.whatsmap.domain.Cctv;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface CctvRepository extends JpaRepository<Cctv, Long> {
-    List<Cctv> findByIdx(Long idx);
-    List<Cctv> findByLatitude(double latitude);
-    List<Cctv> findByLongitude(double longitude);
+public interface CctvRepository extends JpaRepository<Cctv, Double> {
+//    Cctv findByIdx(Long idx);
+    Cctv findByLatitude(Double latitude);
+    List<Cctv> findByLongitude(Double longitude);
+
 }
